d0e5fb829da29bc720d4849ae6b7cfdff66a1113
Keith Penney <kpenney@lbl.gov>
2024-12-06 20:14:22 -0800

    Adding policy file

---
+++ py/policy.py
@@ -0,0 +1,5 @@
+# A simple singleton class to hold policy parameters for code generation
+
+class Policy():
+    registered_rams = True  # IMPLEMENTED
+    registered_wen = True   # TODO NOT IMPLEMENTED
